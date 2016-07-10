2c21eb610f669ab73c1ce4cab2156f1b7f87a71b
Arve Knudsen <arve.knudsen@gmail.com>
2016-07-10 18:07:04 +1000

    Add post

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
   // When running Ghost in the wild, use the production environment.
   // Configure your URL and mail settings here
   production: {
-    url: 'http://muzhack.github.io',
+    url: 'http://blog.muzhack.com',
     mail: {},
     database: {
       client: 'sqlite3',
@@ -31,7 +31,7 @@ config = {
   development: {
     // The url to use when providing links to the site, E.g. in RSS and email.
     // Change this to your Ghost blog's published URL.
-    url: 'http://localhost:2368',
+    url: 'http://blog.muzhack.com',
 
     // Example mail config
     // Visit http://support.ghost.org/mail for instructions
